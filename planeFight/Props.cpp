b25e33bfe50d496df10be4958e73ce63d4fc9c0d
1m188 <228117330@qq.com>
2018-12-17 23:17:42 +0800

    加入道具类，并且能够设置类型。

---
+++ planeFight/Props.cpp
@@ -0,0 +1,11 @@
+#include "Props.h"
+
+Props::Props() :Sprite()
+{
+
+}
+
+Props::~Props()
+{
+
+}
---
+++ planeFight/Props.h
@@ -0,0 +1,40 @@
+#ifndef PROPS_H
+#define PROPS_H
+
+#include "Sprite.h"
+
+//道具类
+class Props :public Sprite
+{
+public:
+	Props();
+	~Props();
+
+	//表示道具类型
+	enum Type
+	{
+		//无
+		None,
+		//炸弹
+		Bomb,
+	};
+
+	//获取每帧的行进距离
+	const int dy() const { return dy_; }
+	//获取每帧的行进距离的引用
+	int &rdy() { return dy_; }
+
+	//获取道具类型的id
+	const Props::Type type() const { return type_; }
+	//获取道具类型的id的引用
+	Props::Type &rtype() { return type_; }
+
+private:
+	//每帧的行进距离
+	int dy_ = 0;
+
+	//标识类型的id
+	Props::Type type_ = Props::Type::None;
+};
+
+#endif // PROPS_H
