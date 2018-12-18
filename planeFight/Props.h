1a67631ff0b04bbe9aa3c43457a61985095e3046
1m188 <228117330@qq.com>
2018-12-18 12:35:43 +0800

    修改了一下代码格式。

--- planeFight/Props.h
+++ planeFight/Props.h
@@ -6,6 +6,10 @@
 //道具类
 class Props :public Sprite
 {
+private:
+	//每帧的行进距离
+	int dy_ = 0;
+
 public:
 	Props();
 	~Props();
@@ -21,20 +25,17 @@ public:
 		Bullet,
 	};
 
-	//获取每帧的行进距离
-	const int dy() const { return dy_; }
-	//获取每帧的行进距离的引用
-	int &rdy() { return dy_; }
-
 	//获取道具类型的id
 	const Props::Type type() const { return type_; }
 	//获取道具类型的id的引用
 	Props::Type &rtype() { return type_; }
 
-private:
-	//每帧的行进距离
-	int dy_ = 0;
+	//获取每帧的行进距离
+	const int dy() const { return dy_; }
+	//获取每帧的行进距离的引用
+	int &rdy() { return dy_; }
 
+private:
 	//标识类型的id
 	Props::Type type_ = Props::Type::None;
 };
