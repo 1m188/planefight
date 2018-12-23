74f4cf0c4e82f37c1431d3a9278fb87013203ca4
1m188 <228117330@qq.com>
2018-12-23 14:40:22 +0800

    添加了plane飞机类，旨在将玩家飞机和敌机统一到一个更加高层的基类，简化游戏逻辑。

---
+++ planeFight/Plane.cpp
@@ -0,0 +1,50 @@
+﻿#include "Plane.h"
+
+Plane::Plane() :Sprite()
+{
+}
+
+Plane::~Plane()
+{
+}
+
+bool Plane::changeImage()
+{
+	//如果生命大于0切换常态图片
+	if (life_ > 0)
+	{
+		//切换常态图片
+		normalImageChangeFpsCounter_++;
+		if (normalImageChangeFpsCounter_ == imageChangeFpsInterval_)
+		{
+			normalImageChangeFpsCounter_ = 0;
+			rimage() = normalImageVector_[nowNormalImageIndex_];
+			nowNormalImageIndex_++;
+			if (nowNormalImageIndex_ >= normalImageVector_.size())
+			{
+				nowNormalImageIndex_ = 0;
+			}
+		}
+	}
+	//否则展示损毁图片
+	else
+	{
+		destroyImageChangeFpsCounter_++;
+		if (destroyImageChangeFpsCounter_ == imageChangeFpsInterval_)
+		{
+			destroyImageChangeFpsCounter_ = 0;
+			//倘若损毁图片都展示完了则从数组中推出去
+			if (nowDestroyImageIndex_ >= destroyImageVector_.size())
+			{
+				return false;
+			}
+			//否则继续展示下一张图片
+			else
+			{
+				rimage() = destroyImageVector_[nowDestroyImageIndex_];
+				nowDestroyImageIndex_++;
+			}
+		}
+	}
+	return true;
+}
---
+++ planeFight/Plane.h
@@ -0,0 +1,102 @@
+﻿#ifndef PLANE_H
+#define PLANE_H
+
+#include "Sprite.h"
+
+//飞机类
+class Plane :public Sprite
+{
+private:
+	//每帧的两个方向上的飞行距离
+	int dx_ = 0;
+	int dy_ = 0;
+
+	//常态图片数组
+	QVector<QPixmap> normalImageVector_;
+	//当前显示常态图片索引
+	int nowNormalImageIndex_ = 0;
+
+	//损毁图片数组
+	QVector<QPixmap> destroyImageVector_;
+	//当前显示损毁图片索引
+	int nowDestroyImageIndex_ = 0;
+
+	//常态图片切换计帧器
+	int normalImageChangeFpsCounter_ = 0;
+	//损毁图片切换计帧器
+	int destroyImageChangeFpsCounter_ = 0;
+	//子弹产生计帧器
+	int productBulletFpsCounter_ = 0;
+
+	//每隔多少帧切换一张常态/损毁图片
+	int imageChangeFpsInterval_ = 0;
+	//每隔多少帧产生子弹
+	int productBulletFpsInterval_ = 0;
+
+	//生命数目
+	int life_ = 1;
+
+public:
+	Plane();
+	~Plane();
+
+	//获取每帧的两个方向上的飞行距离
+	const int dx() const { return dx_; }
+	const int dy() const { return dy_; }
+	//获取每帧的两个方向上的飞行距离的引用
+	int &rdx() { return dx_; }
+	int &rdy() { return dy_; }
+
+	//获取常态/损毁图片数组
+	QVector<QPixmap> normalImageVector() { return normalImageVector_; }
+	QVector<QPixmap> destroyImageVector() { return destroyImageVector_; }
+	//获取常态/损毁图片数组的引用
+	QVector<QPixmap> &rnormalImageVector() { return normalImageVector_; }
+	QVector<QPixmap> &rdestroyImageVector() { return destroyImageVector_; }
+
+	//获取当前显示常态图片索引
+	const int nowNormalImageIndex() const { return nowNormalImageIndex_; }
+	//获取当前显示常态图片索引的引用
+	int &rnowNormalImageIndex() { return nowNormalImageIndex_; }
+	//获取当前显示损毁图片索引
+	const int nowDestroyImageIndex() const { return nowDestroyImageIndex_; }
+	//获取当前显示损毁图片索引的引用
+	int &rnowDestroyImageIndex() { return nowDestroyImageIndex_; }
+
+	//获取常态图片切换计帧器
+	const int normalImageChangeFpsCounter() const { return normalImageChangeFpsCounter_; }
+	//获取常态图片切换计帧器的引用
+	int &rnormalImageChangeFpsCounter() { return normalImageChangeFpsCounter_; }
+
+	//获取损毁图片切换计帧器
+	const int destroyImageChangeFpsCounter() const { return destroyImageChangeFpsCounter_; }
+	//获取损毁图片切换计帧器的引用
+	int &rdestroyImageChangeFpsCounter() { return destroyImageChangeFpsCounter_; }
+
+	//获取每隔多少帧切换一张常态/损毁图片
+	const int imageChangeFpsInterval() const { return imageChangeFpsInterval_; }
+	//获取每隔多少帧切换一张常态/损毁图片的引用
+	int &rimageChangeFpsInterval() { return imageChangeFpsInterval_; }
+
+	//获取子弹产生计帧器
+	const int productBulletFpsCounter() const { return productBulletFpsCounter_; }
+	//获取子弹产生计帧器的引用
+	int &rproductBulletFpsCounter() { return productBulletFpsCounter_; }
+
+	//获取每隔多少帧产生子弹
+	const int productBulletFpsInterval() const { return productBulletFpsInterval_; }
+	//获取每隔多少帧产生子弹的引用
+	int &rproductBulletFpsInterval() { return productBulletFpsInterval_; }
+
+	//获取生命数目
+	const int life() const { return life_; }
+	//获取生命数目引用
+	int &rlife() { return life_; }
+
+	//切换图片
+	//如果飞机没有损毁的话切换图片返回true
+	//否则损毁图片展示完毕后返回false
+	bool changeImage();
+};
+
+#endif // !PLANE_H
