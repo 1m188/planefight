04b96b7651890f96b7f8340385bc282ecc751ccc
1m188 <228117330@qq.com>
2018-12-23 21:43:15 +0800

    在游戏启动的时候加载资源。

--- planeFight/main.cpp
+++ planeFight/main.cpp
@@ -1,10 +1,12 @@
-#include <QtWidgets/QApplication>
+﻿#include <QtWidgets/QApplication>
 #include "Director.h"
 #include "Window.h"
+#include "Image.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	Image::getInstance()->init(); //初始化图片
 	Director::getInstance()->setWindow(new Window());
 	Director::getInstance()->getWindow()->show();
 	return a.exec();
