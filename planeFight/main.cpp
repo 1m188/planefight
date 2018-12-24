d24e1a628c883a652eb09a414fd4e563b3badf61
1m188 <228117330@qq.com>
2018-12-24 23:29:35 +0800

    在游戏开始的时候对数据进行读取，游戏结束的时候对数据进行保存。

--- planeFight/main.cpp
+++ planeFight/main.cpp
@@ -2,12 +2,16 @@
 #include "Director.h"
 #include "Window.h"
 #include "Image.h"
+#include "Config.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	Image::getInstance()->init(); //初始化图片
+	Config::getInstance()->init(); //初始化游戏数据
 	Director::getInstance()->setWindow(new Window());
 	Director::getInstance()->getWindow()->show();
-	return a.exec();
+	a.exec();
+	Config::getInstance()->uninit(); //保存游戏数据
+	return 0;
 }
