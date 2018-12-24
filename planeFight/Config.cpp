a3c3354e33aa373ef18b63d39bb4700b53d8f12a
1m188 <228117330@qq.com>
2018-12-24 23:28:30 +0800

    添加了Config单例全局类，对于某些游戏数据的存储和读取做一个封装，比如帧数等，目前也只有帧数在这个里面。。。

---
+++ planeFight/Config.cpp
@@ -0,0 +1,55 @@
+﻿#include "Config.h"
+#include "QFile"
+
+Config *Config::instance = nullptr; //初始化单例指针
+
+Config::Config()
+{
+}
+
+Config::~Config()
+{
+}
+
+Config * Config::getInstance()
+{
+	if (!instance)
+	{
+		instance = new Config();
+	}
+	return instance;
+}
+
+void Config::init()
+{
+	QFile f("config.ini");
+	//如果设置文件存在则读取数据
+	if (f.exists())
+	{
+		f.open(QIODevice::ReadOnly);
+
+		QString temp;
+
+		//读取fps值
+		temp = f.readLine();
+		temp.chop(1);
+		fps_ = temp.toInt();
+
+		f.close();
+	}
+	//否则创建一个新的文件
+	else
+	{
+		f.open(QIODevice::WriteOnly);
+		f.close();
+	}
+}
+
+void Config::uninit()
+{
+	QFile f("config.ini");
+	f.open(QIODevice::WriteOnly | QIODevice::Truncate);
+	//写入保存fps值
+	f.write((QString::number(fps_) + "\n").toStdString().c_str());
+	f.close();
+}
---
+++ planeFight/Config.h
@@ -0,0 +1,33 @@
+﻿#ifndef CONFIG_H
+#define CONFIG_H
+
+//设置类
+class Config
+{
+private:
+	Config(); //禁止自动默认构造
+
+	static Config *instance; //单例指针
+
+	//帧数
+	int fps_ = 0;
+
+public:
+	Config(const Config &) = delete;
+	Config &operator=(const Config &) = delete;
+	~Config();
+
+	static Config *getInstance(); //获取单例指针
+
+	//初始化
+	void init();
+	//保存数据
+	void uninit();
+
+	//获取帧数
+	const int fps() const { return fps_; }
+	//获取帧数的引用
+	int &rfps() { return fps_; }
+};
+
+#endif // !CONFIG_H
