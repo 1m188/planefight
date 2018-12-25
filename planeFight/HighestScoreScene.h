9b0c2783ec34fbb7dc30a1f85d0144535a53979e
1m188 <228117330@qq.com>
2018-12-25 11:48:41 +0800

    添加了最高分场景，并且能够显示最高分和返回开始场景。

---
+++ planeFight/HighestScoreScene.h
@@ -0,0 +1,19 @@
+﻿#ifndef HIGHESTSCORESCENE_H
+#define HIGHESTSCORESCENE_H
+
+#include "Scene.h"
+
+class HighestScoreScene : public Scene
+{
+public:
+	HighestScoreScene(Window *parent);
+	~HighestScoreScene();
+
+	//初始化场景
+	void init() override;
+
+private slots:
+	void returnToStartSceneButtonClicked(); //单击返回开始场景按钮
+};
+
+#endif // !HIGHESTSCORESCENE_H
