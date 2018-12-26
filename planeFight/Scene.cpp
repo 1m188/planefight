bad8dbde672839d4abd11514ad1fca781990d343
1m188 <228117330@qq.com>
2018-12-26 23:52:09 +0800

    修改了普通场景的菜单和按钮样式，使其可以更加美观。

--- planeFight/Scene.cpp
+++ planeFight/Scene.cpp
@@ -1,9 +1,20 @@
-#include "Scene.h"
+﻿#include "Scene.h"
+#include "Image.h"
 
 Scene::Scene(Window *parent)
 	: QWidget(parent)
 {
+	//设置大小和窗口一样大
 	resize(parent->size());
+
+	//设置背景
+	setAutoFillBackground(true);
+	QPalette pa;
+	pa.setBrush(QPalette::Background, QBrush(Image::getInstance()->backgroundImage.scaled(size())));
+	setPalette(pa);
+
+	//样式表设置按钮颜色和透明度
+	setStyleSheet("QPushButton{background-color:rgba(254,254,254,0.2);}");
 }
 
 Scene::~Scene()
