29c68434e92b1ef5cd7de42ea3de026ba1c326a0
1m188 <228117330@qq.com>
2019-01-06 18:10:18 +0800

    加入了发布版本的组件寻找路径。

--- planeFight/main.cpp
+++ planeFight/main.cpp
@@ -6,6 +6,11 @@
 
 int main(int argc, char *argv[])
 {
+	//发布版本
+#ifdef NDEBUG
+	QApplication::addLibraryPath("./plugins");
+#endif // NDEBUG
+
 	QApplication a(argc, argv);
 	Image::getInstance()->init(); //初始化图片
 	Config::getInstance()->init(); //初始化游戏数据
